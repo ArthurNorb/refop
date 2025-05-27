a127acb8711b442b5070f7cfcea6fda17d189f06
ArthurNorb <arthurnorberto4@gmail.com>
2025-05-27 15:56:52 +0000

    header e body estilizados

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> {{-- xupa upa --}}
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -7,15 +7,35 @@
 
     <title>@yield('title', config('app.name', 'REFOP'))</title>
 
-    <!-- Styles / Scripts -->
+    {{-- Icons --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- Styles / Scripts --}}
+    <link rel="stylesheet" href="{{ asset('css/background.css') }}">
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 
 <body class="d-flex flex-column min-vh-100 bg-off-white">
 
-    <header class="bg-azul-escuro text-light p-5 text-center">
-        <h1>REFOP</h1>
-        <p>Associação das Repúblicas Federais de Ouro Preto</p>
+    <header class="bg-azul-escuro text-light py-3 px-5">
+        <div class="d-flex align-items-center justify-content-start">
+            <img src="img/LogoREFOP.png" alt="REFOP" style="width: 8rem; height: auto; margin-right: 1rem">
+            <div>
+                <h1>REFOP</h1>
+                <p>Associação das Repúblicas Federais de Ouro Preto</p>
+            </div>
+
+            <div class="ms-auto w-25">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
     </header>
 
     <nav>
@@ -41,11 +61,13 @@
         </ul>
     </nav>
 
-    <main class="bg-off-white h-100">
+    <main class="h-100">
         <div class="w-50 mx-auto">
             @yield('content')
         </div>
     </main>
+
+    <div id="igreja-decorativa-fundo"></div>
 
     <footer class="footer mt-auto py-3 bg-azul-escuro">
         <div class="container text-center">
